53db07da5cc541a9a0a099bbba0ccd6a3211b216
ntsmithcs <ntsmithcs@gmail.com>
2025-05-25 15:02:26 -0400

    player is moving around the screen

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -18,5 +18,22 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
     
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    # tfgh instead of wasd
     def update(self, dt):
-        pass
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_f]:
+            self.rotate(-dt)
+        if keys[pygame.K_h]:
+            self.rotate(dt)
+        if keys[pygame.K_t]:
+            self.move(dt)
+        if keys[pygame.K_g]:
+            self.move(-dt)
+
